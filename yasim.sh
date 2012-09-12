a53c8c78126cdd197852dc8c02fadb6e03fd4f11
Anton Komarov <anton.komarov@gmail.com>
2012-09-12 17:52:04 +0400

    Added database and namespace routines.

--- yasim.sh
+++ yasim.sh
@@ -14,3 +14,146 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+help_usage() {
+
+echo "	
+Yasim usage
+	--help, -h		show this information
+
+Database
+	--init			database initialize
+	--dbscheme		database scheme (mandatory)
+	--dbfile		database filename (mandatory)
+
+Namespaces
+	--add-ns		add new namespace
+	--expire-ns		expire existing namespace
+	--show-all-ns		show all namespaces
+	--show-active-ns	show active namespaces
+	--show-expired-ns	show expired namespaces
+	--ns-id			namespace id (mandatory)
+	--ns-name		namespace name (mandatory)
+	--ns-desc		namespace description (mandatory)
+	--ns-btime		namespace begin of use time (optional)
+	--ns-etime		namespace expire of use time (optional)
+	--ns-tsn-id		namespace transaction id to expire (mandatory)
+"
+}
+
+#Global funcs
+
+init_db() {
+
+	[ "X$db_file" == "X" ] && exit 1
+	[ -f $db_file ] && rm $db_file
+	[ -f $db_scheme ] && sqlite3 $db_file < $db_scheme || echo "Database scheme not exists!"
+
+}
+
+check_db () {
+
+	msg="Init db: ./yasim.sh --init --db-scheme <db-scheme-file> --db-file <db-filename>"
+	[ -f $db_file ] || echo $msg
+
+}
+
+#Namespace funcs
+
+add_ns () {
+
+	[ "X$ns_id" == "X" ] && exit 1
+	[ "X$ns_name" == "X" ] && exit 1
+	[ "X$ns_desc" == "X" ] && exit 1
+	[ "X$ns_btime" == "X" ] && ns_btime=$(date "+%Y-%m-%d %H:%M:%S.000")
+	q="insert or rollback into namespaces (ns_id,ns_name,ns_desc,ns_btime)"
+	q="$q values ($ns_id,'$ns_name','$ns_desc','$ns_btime');"
+	sqlite3 $db_file "$q"
+
+}
+
+expire_ns () {
+
+	[ "X$ns_tsn_id" == "X" ] && exit 1
+	[ "X$ns_etime" == "X" ] && ns_etime=$(date "+%Y-%m-%d %H:%M:%S.000")
+	q="insert or rollback into ns_exp (id,ns_etime) values ($ns_tsn_id,'$ns_etime');"
+	sqlite3 $db_file "$q"
+
+}
+
+show_all_ns () {
+
+	q="select * from namespaces order by ns_btime desc"
+	sqlite3 $db_file "$q"
+}
+
+show_active_ns () {
+
+	q="select namespaces.* from namespaces,ns_exp"
+	q="$q where namespaces.id!=ns_exp.id"
+	q="$q order by ns_btime desc"
+	sqlite3 $db_file "$q"
+}
+
+show_expired_ns () {
+
+	q="select namespaces.*, ns_exp.ns_etime from namespaces, ns_exp"
+	q="$q where namespaces.id==ns_exp.id and ns_exp.ns_etime > datetime('now')"
+	q="$q order by ns_exp.ns_etime desc"
+	sqlite3 $db_file "$q"
+}
+
+shortopts="h"
+
+#global keys
+longopts="help,init,db-file:,db-scheme:"
+
+#namespace keys
+longopts="$longopts,add-ns,expire-ns,show-all-ns,show-active-ns,show-expired-ns"
+longopts="$longopts,ns-id:,ns-name:,ns-desc:,ns-btime:,ns-etime:,ns-tsn-id:"
+
+t=$(getopt -o $shortopts --long $longopts -n 'yasim' -- "$@")
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+eval set -- "$t"
+
+while true ; do
+	case "$1" in
+		-h|--help) help_usage ; break ;;
+		--db-scheme) db_scheme=$2; shift 2;;
+		--db-file) db_file=$2; shift 2;;
+		--init) init_db=1; shift ;;
+		--ns-id) ns_id=$2; shift 2;;
+		--ns-name) ns_name=$2; shift 2;;
+		--ns-desc) ns_desc=$2; shift 2;;
+		--ns-btime) ns_btime=$2; shift 2;;
+		--ns-etime) ns_etime=$2; shift 2;;
+		--ns-tsn-id) ns_tsn_id=$2; shift 2;;
+		--add-ns) add_ns=1; shift;;
+		--expire-ns) expire_ns=1; shift;;
+		--show-all-ns) show_all_ns=1; shift;;
+		--show-active-ns) show_active_ns=1; shift;;
+		--show-expired-ns) show_expired_ns=1; shift;;
+		--) shift ; break ;;
+		*) echo "Internal error!" ; exit 1 ;;
+	esac
+done
+
+#Global block
+
+[ "$init_db" == 1 ] && init_db || check_db
+
+
+#Namespace block
+
+[ "$add_ns" == 1 ] && add_ns
+
+[ "$expire_ns" == 1 ] && expire_ns
+
+[ "$show_all_ns" == 1 ] && show_all_ns
+
+[ "$show_active_ns" == 1 ] && show_active_ns
+
+[ "$show_expired_ns" == 1 ] && show_expired_ns
