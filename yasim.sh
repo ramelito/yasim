2fabb10e41db558e5ffc606689a9f0527c9a9d8e
Anton Komarov <anton.komarov@gmail.com>
2012-09-12 19:29:17 +0400

    Added user roles routines.

--- yasim.sh
+++ yasim.sh
@@ -16,6 +16,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+sqlite3opts="-header"
+
 help_usage() {
 
 echo "	
@@ -24,8 +26,8 @@ Yasim usage
 
 Database
 	--init			database initialize
-	--dbscheme		database scheme (mandatory)
-	--dbfile		database filename (mandatory)
+	--db-scheme		database scheme (mandatory)
+	--db-file		database filename (mandatory)
 
 Namespaces
 	--add-ns		add new namespace
@@ -39,6 +41,19 @@ Namespaces
 	--ns-btime		namespace begin of use time (optional)
 	--ns-etime		namespace expire of use time (optional)
 	--ns-tsn-id		namespace transaction id to expire (mandatory)
+
+User roles (with valid ns-id)
+        --add-ur                add new user role
+        --expire-ur             expire existing user role
+        --show-all-ur           show all user roles
+        --show-active-ur        show active user roles
+        --show-expired-ur       show expired user roles
+        --ur-id                 user role id (mandatory)
+        --ur-name               user role name (mandatory)
+        --ur-desc               user role description (mandatory)
+        --ur-btime              user role begin of use time (optional)
+        --ur-etime              user role expire of use time (optional)
+        --ur-tsn-id             user role transaction id to expire (mandatory)
 "
 }
 
@@ -59,6 +74,14 @@ check_db () {
 
 }
 
+check_ns () {
+
+	msg="This operation works only with valid ns-id: ./yasim.sh --ns-id <ns_id>\n"
+	msg="${msg}Valid ns-id can be found with command: ./yasim.sh --show-active-ns"
+	[ "X$ns_id" == "X" ] && echo -e $msg
+	[ "X$ns_id" == "X" ] && exit 1
+}
+
 #Namespace funcs
 
 add_ns () {
@@ -66,42 +89,98 @@ add_ns () {
 	[ "X$ns_id" == "X" ] && exit 1
 	[ "X$ns_name" == "X" ] && exit 1
 	[ "X$ns_desc" == "X" ] && exit 1
-	[ "X$ns_btime" == "X" ] && ns_btime=$(date "+%Y-%m-%d %H:%M:%S.000")
+	[ "X$ns_btime" == "X" ] && ns_btime=$(date "+%Y-%m-%d %H:%M:%S")
 	q="insert or rollback into namespaces (ns_id,ns_name,ns_desc,ns_btime)"
 	q="$q values ($ns_id,'$ns_name','$ns_desc','$ns_btime');"
-	sqlite3 $db_file "$q"
+	sqlite3 $sqlite3opts $db_file "$q"
 
 }
 
 expire_ns () {
 
 	[ "X$ns_tsn_id" == "X" ] && exit 1
-	[ "X$ns_etime" == "X" ] && ns_etime=$(date "+%Y-%m-%d %H:%M:%S.000")
+	[ "X$ns_etime" == "X" ] && ns_etime=$(date "+%Y-%m-%d %H:%M:%S")
 	q="insert or rollback into ns_exp (id,ns_etime) values ($ns_tsn_id,'$ns_etime');"
-	sqlite3 $db_file "$q"
+	sqlite3 $sqlite3opts $db_file "$q"
 
 }
 
 show_all_ns () {
 
 	q="select * from namespaces order by ns_btime desc"
-	sqlite3 $db_file "$q"
+	sqlite3 $sqlite3opts $db_file "$q"
 }
 
 show_active_ns () {
 
-	q="select namespaces.* from namespaces,ns_exp"
-	q="$q where namespaces.id!=ns_exp.id"
+	q="select * from namespaces"
+	q="$q where id not in"
+	q="$q (select id from ns_exp)"
+	q="$q and namespaces.ns_btime < datetime('now','localtime')"
 	q="$q order by ns_btime desc"
-	sqlite3 $db_file "$q"
+	sqlite3 $sqlite3opts $db_file "$q"
 }
 
 show_expired_ns () {
 
 	q="select namespaces.*, ns_exp.ns_etime from namespaces, ns_exp"
-	q="$q where namespaces.id==ns_exp.id and ns_exp.ns_etime > datetime('now')"
+	q="$q where namespaces.id==ns_exp.id and ns_exp.ns_etime < datetime('now','localtime')"
 	q="$q order by ns_exp.ns_etime desc"
-	sqlite3 $db_file "$q"
+	sqlite3 $sqlite3opts $db_file "$q"
+}
+
+#User roles funcs
+
+add_ur () {
+	
+	check_ns
+        [ "X$ur_id" == "X" ] && exit 1
+        [ "X$ur_name" == "X" ] && exit 1
+        [ "X$ur_desc" == "X" ] && exit 1
+        [ "X$ur_btime" == "X" ] && ur_btime=$(date "+%Y-%m-%d %H:%M:%S")
+        q="insert or rollback into user_roles (ur_id,ur_name,ur_desc,ur_btime,ns_id)"
+        q="$q values ($ur_id,'$ur_name','$ur_desc','$ur_btime',$ns_id);"
+        sqlite3 $sqlite3opts $db_file "$q"
+
+}
+
+expire_ur () {
+
+        [ "X$ur_tsn_id" == "X" ] && exit 1
+        [ "X$ur_etime" == "X" ] && ur_etime=$(date "+%Y-%m-%d %H:%M:%S")
+        q="insert or rollback into ur_exp (id,ur_etime) values ($ur_tsn_id,'$ur_etime');"
+        sqlite3 $sqlite3opts $db_file "$q"
+
+}
+
+show_all_ur () {
+
+	check_ns
+        q="select * from user_roles where ns_id=$ns_id order by ur_btime desc"
+        sqlite3 $sqlite3opts $db_file "$q"
+}
+
+show_active_ur () {
+
+	check_ns
+        q="select * from user_roles"
+        q="$q where id not in"
+	q="$q (select id from ur_exp)"
+	q="$q and user_roles.ur_btime < datetime('now','localtime')"
+	q="$q and ns_id=$ns_id"
+        q="$q order by ur_btime desc"
+        sqlite3 $sqlite3opts $db_file "$q"
+}
+
+show_expired_ur () {
+
+	check_ns
+        q="select user_roles.*, ur_exp.ur_etime from user_roles, ur_exp"
+        q="$q where user_roles.id==ur_exp.id"
+	q="$q and ur_exp.ur_etime < datetime('now','localtime')"
+	q="$q and user_roles.ns_id=$ns_id"
+        q="$q order by ur_exp.ur_etime desc"
+        sqlite3 $sqlite3opts $db_file "$q"
 }
 
 shortopts="h"
@@ -112,6 +191,10 @@ longopts="help,init,db-file:,db-scheme:"
 #namespace keys
 longopts="$longopts,add-ns,expire-ns,show-all-ns,show-active-ns,show-expired-ns"
 longopts="$longopts,ns-id:,ns-name:,ns-desc:,ns-btime:,ns-etime:,ns-tsn-id:"
+
+#user roles keys
+longopts="$longopts,add-ur,expire-ur,show-all-ur,show-active-ur,show-expired-ur"
+longopts="$longopts,ur-id:,ur-name:,ur-desc:,ur-btime:,ur-etime:,ur-tsn-id:"
 
 t=$(getopt -o $shortopts --long $longopts -n 'yasim' -- "$@")
 
@@ -136,6 +219,17 @@ while true ; do
 		--show-all-ns) show_all_ns=1; shift;;
 		--show-active-ns) show_active_ns=1; shift;;
 		--show-expired-ns) show_expired_ns=1; shift;;
+		--ur-id) ur_id=$2; shift 2;;
+                --ur-name) ur_name=$2; shift 2;;
+                --ur-desc) ur_desc=$2; shift 2;;
+                --ur-btime) ur_btime=$2; shift 2;;
+                --ur-etime) ur_etime=$2; shift 2;;
+                --ur-tsn-id) ur_tsn_id=$2; shift 2;;
+                --add-ur) add_ur=1; shift;;
+                --expire-ur) expire_ur=1; shift;;
+                --show-all-ur) show_all_ur=1; shift;;
+                --show-active-ur) show_active_ur=1; shift;;
+                --show-expired-ur) show_expired_ur=1; shift;;
 		--) shift ; break ;;
 		*) echo "Internal error!" ; exit 1 ;;
 	esac
@@ -157,3 +251,15 @@ done
 [ "$show_active_ns" == 1 ] && show_active_ns
 
 [ "$show_expired_ns" == 1 ] && show_expired_ns
+
+#User roles block
+
+[ "$add_ur" == 1 ] && add_ur
+
+[ "$expire_ur" == 1 ] && expire_ur
+
+[ "$show_all_ur" == 1 ] && show_all_ur
+
+[ "$show_active_ur" == 1 ] && show_active_ur
+
+[ "$show_expired_ur" == 1 ] && show_expired_ur
